798248bb76549e601975f6dfdb6ad0aec751c184
Jan Hovland <jan.hovland@lyse-net>
2019-10-16 11:32:11 +0200

    Update

--- PersonView/WelcomeView.swift
+++ PersonView/WelcomeView.swift
@@ -10,12 +10,25 @@ import SwiftUI
 
 struct WelcomeView: View {
     var body: some View {
+        /*
         Text("Welcome to PersonView")
             .font(.system(size: 60))
             .bold()
             .foregroundColor(.red)
             .lineLimit(2)
             .multilineTextAlignment(.leading)
+         */
+        
+        Image("Cloudkit")
+            .resizable()
+            .scaledToFit()
+            .aspectRatio(3/4, contentMode: .fill)
+            // .aspectRatio(1/1, contentMode: .fill)
+            .edgesIgnoringSafeArea(.all)
+            .saturation(0.75)
+            .blur(radius: 3)
+            .opacity(0.35)
+        
     }
 }
 
