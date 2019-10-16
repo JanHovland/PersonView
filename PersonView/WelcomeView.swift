3f39df07b113271bb00d48a7a26b57edb46d4dac
Jan Hovland <jan.hovland@lyse-net>
2019-10-16 12:50:26 +0200

    Update

--- PersonView/WelcomeView.swift
+++ PersonView/WelcomeView.swift
@@ -10,28 +10,36 @@ import SwiftUI
 
 struct WelcomeView: View {
     
+    @State var eMail: String = ""
     @State var name: String = ""
 
     var body: some View {
         VStack (content: {
            WelcomeMessageView()
-           
-           TextField("Type your name", text: $name)
-            .padding(EdgeInsets(top: 8, leading: 16,
-                                bottom: 8, trailing: 16))
-            .background(Color.white)
-            .overlay(
-              RoundedRectangle(cornerRadius: 8)
-                .stroke(lineWidth: 2)
-                .foregroundColor(.blue)
-            )
+           VStack {
+               TextField("Type your name", text: $name)
+                    .padding(EdgeInsets(top: 8, leading: 16,
+                                    bottom: 8, trailing: 16))
+                    .background(Color.white)
+                    .overlay(RoundedRectangle(cornerRadius: 8)
+                    .stroke(lineWidth: 2)
+                    .foregroundColor(.blue)
+                )
+                
+                TextField("Type your email", text: $eMail)
+                     .padding(EdgeInsets(top: 8, leading: 16,
+                                     bottom: 8, trailing: 16))
+                     .background(Color.white)
+                     .overlay(RoundedRectangle(cornerRadius: 8)
+                     .stroke(lineWidth: 2)
+                     .foregroundColor(.blue)
+                 )
+            }
             .shadow(color: Color.gray.opacity(0.4),
                     radius: 3, x: 1, y: 2)
-            
         })
         .padding()
         .background(WelcomeBackgroundImage())
-        
     }
 }
 
