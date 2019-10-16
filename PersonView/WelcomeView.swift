744b9c5376c705b4eea6b8af6812ca7bb205971a
Jan Hovland <jan.hovland@lyse-net>
2019-10-16 20:50:41 +0200

    Update

--- PersonView/WelcomeView.swift
+++ PersonView/WelcomeView.swift
@@ -16,27 +16,29 @@ struct WelcomeView: View {
     var body: some View {
         VStack (content: {
            WelcomeMessageView()
-           VStack {
-               TextField("Type your name", text: $name)
-                    .padding(EdgeInsets(top: 8, leading: 16,
-                                    bottom: 8, trailing: 16))
-                    .background(Color.white)
-                    .overlay(RoundedRectangle(cornerRadius: 8)
-                    .stroke(lineWidth: 2)
-                    .foregroundColor(.blue)
-                )
-                
-                TextField("Type your email", text: $eMail)
+           TextField("Type your name", text: $name)
                      .padding(EdgeInsets(top: 8, leading: 16,
-                                     bottom: 8, trailing: 16))
+                                         bottom: 8, trailing: 16))
                      .background(Color.white)
-                     .overlay(RoundedRectangle(cornerRadius: 8)
-                     .stroke(lineWidth: 2)
-                     .foregroundColor(.blue)
-                 )
-            }
-            .shadow(color: Color.gray.opacity(0.4),
-                    radius: 3, x: 1, y: 2)
+                     .overlay(
+                       RoundedRectangle(cornerRadius: 8)
+                         .stroke(lineWidth: 2)
+                         .foregroundColor(.blue)
+                     )
+                     .shadow(color: Color.gray.opacity(0.4),
+                             radius: 3, x: 1, y: 2)
+            TextField("Type your email", text: $eMail)
+                      .padding(EdgeInsets(top: 8, leading: 16,
+                                          bottom: 8, trailing: 16))
+                      .background(Color.white)
+                      .overlay(
+                        RoundedRectangle(cornerRadius: 8)
+                          .stroke(lineWidth: 2)
+                          .foregroundColor(.blue)
+                      )
+                      .shadow(color: Color.gray.opacity(0.4),
+                              radius: 3, x: 1, y: 2)
+                      .keyboardType(.emailAddress)
         })
         .padding()
         .background(WelcomeBackgroundImage())
